1a2e74a24198f6ad1ffa5133a1f141069abb70c7
Heaven Silay <heavensilay@gmail.com>
2025-10-18 03:39:03 +0800

    test commit

--- Focus/src/com/focus/core/StudySession.java
+++ Focus/src/com/focus/core/StudySession.java
@@ -1,5 +1,14 @@
 package com.focus.core;
 
-public class StudySession {
+import java.util.Scanner;
 
+public class StudySession {
+    public void startSession() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter study duration (in minutes): ");
+        int minutes = scanner.nextInt();
+        System.out.println("Starting focus timer for " + minutes + " minutes...");
+        
+        //test
+    }
 }
--- Focus/src/com/focus/main/FocusApp.java
+++ Focus/src/com/focus/main/FocusApp.java
@@ -1,10 +1,13 @@
 package com.focus.main;
 
+import com.focus.core.StudySession;
+
 public class FocusApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to F.ocus — Your Study Companion!");
+        System.out.println("-------------------------------------------");
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+        StudySession session = new StudySession();
+        session.startSession();
+    }
 }
