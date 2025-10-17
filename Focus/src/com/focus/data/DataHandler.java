21b859752d2bd2f49c3f0ee6eeb4d216281cd722
Heaven Silay <heavensilay@gmail.com>
2025-10-18 02:56:18 +0800

    Initial commit

---
+++ Focus/src/com/focus/data/DataHandler.java
@@ -0,0 +1,5 @@
+package com.focus.data;
+
+public class DataHandler {
+
+}
---
+++ Focus/src/com/focus/main/FocusApp.java
@@ -0,0 +1,10 @@
+package com.focus.main;
+
+public class FocusApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
