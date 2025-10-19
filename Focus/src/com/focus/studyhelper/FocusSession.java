e343bd60b9955df64105f0804e9aab2f86129960
Heaven Silay <heavensilay@gmail.com>
2025-10-20 02:58:28 +0800

    focus session update

--- Focus/src/com/focus/studyhelper/FocusSession.java
+++ Focus/src/com/focus/studyhelper/FocusSession.java
@@ -1,5 +1,24 @@
 package com.focus.studyhelper;
 
 public class FocusSession {
+	private StudyMethod method;
+    private int duration; // in minutes
+    private FocusTimer timer;
 
+    public FocusSession(StudyMethod method) {
+        this.method = method;
+        this.timer = new FocusTimer();
+    }
+
+    public void start(int duration) {
+        this.duration = duration;
+        System.out.println("Starting session using: " + method.getName());
+        timer.start(duration);
+        System.out.println("Session complete!");
+    }
+
+    @Override
+    public String toString() {
+        return "Method: " + method.getName() + " | Duration: " + duration + " mins";
+    }
 }
