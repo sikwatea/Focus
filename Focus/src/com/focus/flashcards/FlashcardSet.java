d7d5890c264d61f7284a2df5ed58f2e8a6792c69
Heaven Silay <heavensilay@gmail.com>
2025-12-18 05:07:07 +0800

    quisel commits final

--- Focus/src/com/focus/flashcards/FlashcardSet.java
+++ Focus/src/com/focus/flashcards/FlashcardSet.java
@@ -3,31 +3,51 @@ package com.focus.flashcards;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FlashcardSet {
+interface FlashcardRepository {
+    void addFlashcard(Flashcard card);
+    boolean removeFlashcard(int index);
+    boolean editFlashcard(int index, String question, String answer);
+    List<Flashcard> getCards();
+    int size();
+}
+
+public class FlashcardSet implements FlashcardRepository {
     private String name;
-    private List<Flashcard> cards = new ArrayList<>();
+    private List<Flashcard> cards;
+
+    public FlashcardSet() {
+        this.name = "";
+        this.cards = new ArrayList<>();
+    }
 
     public FlashcardSet(String name) {
         this.name = name;
+        this.cards = new ArrayList<>();
     }
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
+    @Override
     public List<Flashcard> getCards() { return cards; }
 
-    public void addFlashcard(Flashcard card) { cards.add(card); }
+    @Override
+    public void addFlashcard(Flashcard card) {
+        cards.add(card);
+    }
 
+    @Override
     public boolean removeFlashcard(int index) {
-        if(index >= 0 && index < cards.size()) {
+        if (index >= 0 && index < cards.size()) {
             cards.remove(index);
             return true;
         }
         return false;
     }
 
+    @Override
     public boolean editFlashcard(int index, String question, String answer) {
-        if(index >= 0 && index < cards.size()) {
+        if (index >= 0 && index < cards.size()) {
             Flashcard c = cards.get(index);
             c.setQuestion(question);
             c.setAnswer(answer);
@@ -36,5 +56,8 @@ public class FlashcardSet {
         return false;
     }
 
-    public int size() { return cards.size(); }
+    @Override
+    public int size() {
+        return cards.size();
+    }
 }
